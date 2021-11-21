0232729ed756c9cca65b43061901c9a765ac475c
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-11-21 01:08:15 +0100

    Section 5: Hibernate with MySQL - Hibernate DDL Update Modes; - MySQL Spring Boot Configuration; - Integration Test for MySQL; - H2 MySQL Compatibility Mode; - Schema Initialization with Hibernate; - Schema Integration with MySQL - Using H2 for Spring Boot Application

--- src/main/java/com/fisnikshabani/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/com/fisnikshabani/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,8 +3,10 @@ package com.fisnikshabani.sdjpaintro.bootstrap;
 import com.fisnikshabani.sdjpaintro.domain.Book;
 import com.fisnikshabani.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({"local","default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -16,6 +18,8 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+
+        bookRepository.deleteAll(); //delete all records and initialize again!
 
         Book book1 = new Book("Domain Driven Desing", "123", "RandomHouse");
         bookRepository.save(book1);
--- src/test/java/com/fisnikshabani/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/com/fisnikshabani/sdjpaintro/MySQLIntegrationTest.java
@@ -1,47 +1,27 @@
 package com.fisnikshabani.sdjpaintro;
 
-import com.fisnikshabani.sdjpaintro.domain.Book;
 import com.fisnikshabani.sdjpaintro.repositories.BookRepository;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("local")
 @DataJpaTest
 @ComponentScan(basePackages = {"package com.fisnikshabani.sdjpaintro.bootstrap"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class SpringBootJpaTestSlice {
+public class MySQLIntegrationTest {
 
     @Autowired
     BookRepository bookRepository;
 
-    @Commit
-    @Order(1)
     @Test
-    void testJpaSlice() {
-
+    void testMySQL() {
         long countBefore = bookRepository.count();
         assertThat(countBefore).isEqualTo(2);
-
-        bookRepository.save(new Book("MyBook","123221", "Self"));
-
-        long countAfter = bookRepository.count();
-        assertThat(countBefore).isLessThan(countAfter);
-    }
-
-    @Order(2)
-    @Test
-    void testJpaSliceTransaction() {
-
-        long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(3);
     }
 }
