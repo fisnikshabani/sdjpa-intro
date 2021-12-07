693cf4943d31140c61fc8f63a972b4de55b775dc
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-12-07 11:37:26 +0100

    Section 8: Hibernate primary keys - Natural keys - Added a new class Booknatural; - Added BookNaturalRepository interface; - Created a new table - book_natural - Integration tests

---
+++ src/main/java/com/fisnikshabani/sdjpaintro/repositories/BookNaturalRepository.java
@@ -0,0 +1,9 @@
+package com.fisnikshabani.sdjpaintro.repositories;
+
+import com.fisnikshabani.sdjpaintro.domain.BookNatural;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookNaturalRepository extends JpaRepository<BookNatural, String> {
+
+
+}
--- src/test/java/com/fisnikshabani/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/com/fisnikshabani/sdjpaintro/MySQLIntegrationTest.java
@@ -1,6 +1,12 @@
 package com.fisnikshabani.sdjpaintro;
 
+import com.fisnikshabani.sdjpaintro.domain.AuthorUuid;
+import com.fisnikshabani.sdjpaintro.domain.BookNatural;
+import com.fisnikshabani.sdjpaintro.domain.BookUuid;
+import com.fisnikshabani.sdjpaintro.repositories.AuthorUuidRepository;
+import com.fisnikshabani.sdjpaintro.repositories.BookNaturalRepository;
 import com.fisnikshabani.sdjpaintro.repositories.BookRepository;
+import com.fisnikshabani.sdjpaintro.repositories.BookUuidRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,9 +25,49 @@ public class MySQLIntegrationTest {
     @Autowired
     BookRepository bookRepository;
 
+    @Autowired
+    AuthorUuidRepository authorUuidRepository;
+
+    @Autowired
+    BookUuidRepository bookUuidRepository;
+
+    @Autowired
+    BookNaturalRepository bookNaturalRepository;
+
+    @Test
+    void bookNaturalTest() {
+        BookNatural bookNatural = new BookNatural();
+        bookNatural.setTitle("My Book");
+        BookNatural saved = bookNaturalRepository.save(bookNatural);
+
+        BookNatural fetched = bookNaturalRepository.getById(saved.getTitle());
+        assertThat(fetched).isNotNull();
+    }
+
+    @Test
+    void testBookUuid() {
+        BookUuid bookUuid = bookUuidRepository.save(new BookUuid());
+        assertThat(bookUuid).isNotNull();
+        assertThat(bookUuid.getId());
+
+        BookUuid fetched = bookUuidRepository.getById(bookUuid.getId());
+        assertThat(fetched).isNotNull();
+    }
+
+    @Test
+    void testAuthorUuid() {
+        AuthorUuid authorUuid = authorUuidRepository.save(new AuthorUuid());
+        assertThat(authorUuid).isNotNull();
+        assertThat(authorUuid.getId()).isNotNull();
+
+        AuthorUuid fetched = authorUuidRepository.getById(authorUuid.getId());
+        assertThat(fetched).isNotNull();
+    }
+
     @Test
     void testMySQL() {
         long countBefore = bookRepository.count();
         assertThat(countBefore).isEqualTo(2);
     }
+
 }
